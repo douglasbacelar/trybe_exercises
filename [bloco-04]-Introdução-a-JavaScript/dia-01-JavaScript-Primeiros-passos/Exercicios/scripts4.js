2ecd2029f6ac2374f4c2be1c9b32c12b3926264d
Douglas Bacelar <bacelardouglas@outlook.com>
2022-10-20 18:30:12 -0300

    Revolvendo exercicios

---
+++ [bloco-04]-Introdução-a-JavaScript/dia-01-JavaScript-Primeiros-passos/Exercicios/scripts4.js
@@ -0,0 +1,43 @@
+const a = 10;
+const b = 15;
+const c = 20;
+
+// Parte 1
+console.log('Soma:' + (a+b));
+console.log('Subtracao:' + (a-b));
+console.log('Multiplicacao:' + (a*b));
+console.log('Divisao:' + (a/b));
+console.log('Modulo:' + (a%b));
+
+// Parte 2
+
+if (a>b) {
+  console.log("'a' é maior que 'b'");
+} else {
+  console.log("'a' é menor que 'b'");
+};
+
+// Parte 3
+
+if (a>b && a>c) {
+  console.log("a' é maior que 'b e c'");
+} else if (b>a && b>c) {
+  console.log("'b é maior que 'a e c'");
+} else (c>a && c>b); {
+  console.log("'c' é maior que 'a e b'");
+}
+
+// Parte 4 
+
+const numero = 16
+
+if (numero > 0) {
+  console.log("Positive"); 
+} else if (numero < 0) {
+  console.log("Negative")
+} else {
+  console.log("Zero");
+};
+
+// Parte 5
+
