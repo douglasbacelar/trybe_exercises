b0969862360b507f4e0a0e47806b3443ade326bf
Douglas Bacelar <bacelardouglas@outlook.com>
2022-10-21 09:23:04 -0300

    Adicionando exercicios finais do dia 1

--- [bloco-04]-Introdução-a-JavaScript/dia-01-JavaScript-Primeiros-passos/Exercicios/scripts4.js
+++ [bloco-04]-Introdução-a-JavaScript/dia-01-JavaScript-Primeiros-passos/Exercicios/scripts4.js
@@ -132,3 +132,49 @@ if ((r % 2 === 0 || t % 2 === 0 || y % 2 === 0)) {
 console.log(code);
 
 // Parte 10
+
+
+const costOfProduct = 1;
+const saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
+
+// Parte 11
+
+
+let aliquotINSS;
+let aliquotIR;
+
+const grossSalary = 2000.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+const baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
